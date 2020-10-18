46562bd9c468c92c36ebbe7349e9e46c6925d852
Adrien Carbonne <adrienc@gmail.com>
2020-10-18 18:51:29 +0200

    Original file from Microsoft Documentation
    Found at: https://docs.microsoft.com/en-us/microsoftteams/migrate-slack-to-teams#channel-files

---
+++ Slack_Export_Channel_Files.ps1
@@ -0,0 +1,109 @@
+$ExportPath = ".\slackHistory"
+$ExportContents = Get-ChildItem -path $ExportPath -Recurse
+Function Get-TimeStamp {
+    return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+}
+
+class File {
+    [string] $Name
+    [string] $Title
+    [string] $Channel
+    [string] $DownloadURL
+    [string] $MimeType
+    [double] $Size
+    [string] $ParentPath
+    [string] $Time
+}
+
+$channelList = Get-Content -Raw -Path .\slackHistory\channels.json | ConvertFrom-Json
+$Files = New-Object -TypeName System.Collections.ObjectModel.Collection["File"]
+
+Write-Host -ForegroundColor Green "$(Get-TimeStamp) Starting Step 1 (processing channel export for files) of 2. Total Channel Count: $($channelList.Count)"
+#Iterate through each Channel listed in the Archive
+foreach ($channel in $channelList) {
+    #Iterate through Channel folders from the Export
+    foreach ($folder in $ExportContents)
+    {
+        #If Channel Name matches..
+        if ($channel.name -eq $folder){
+            $channelJsons = Get-ChildItem -Path $folder.FullName -File
+            Write-Host -ForegroundColor White "$(Get-TimeStamp) Info: Starting to process $($channelJsons.Count) days of content for #$($channel.name)."
+            #Start processing the daily JSON for files
+            foreach ($json in $channelJsons){
+                $currentJson = Get-Content -Raw -Path $json.FullName | ConvertFrom-Json
+                #Write-Host -ForegroundColor Yellow "$(Get-TimeStamp) Info: Processing $($json.Name) in #$($channel.name).."
+                #Iterate through every action
+                foreach ($entry in $currentJson){
+                    #If the action contained file(s)..
+                    if($null -ne $entry.files){
+                        #Iterate through each file and add it to the List of Files to download
+                        foreach ($item in $entry.Files) {
+							$file = New-Object -TypeName File
+                            if ($null -ne $item.url_private_download){
+                                $file.Name = $item.name
+                                $file.Title = $item.Title
+                                $file.Channel = $channel.name
+                                $file.DownloadURL = $item.url_private_download
+                                $file.MimeType = $item.mimetype
+                                $file.Size = $item.size
+                                $file.ParentPath = $folder.FullName
+                                $file.Time = $item.created
+                                $files.Add($file)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+Write-Host -ForegroundColor Green "$(Get-TimeStamp) Step 1 of 2 complete. `n"
+
+Write-Host -ForegroundColor Green "$(Get-TimeStamp) Starting step 2 (creating folders and downloading files) of 2."
+#Determine which Files folders need to be created
+$FoldersToMake = New-Object System.Collections.ObjectModel.Collection["string"]
+foreach ($file in $files){
+    if ($FoldersToMake -notcontains $file.Channel){
+        $FoldersToMake.Add($file.Channel)
+    }
+}
+
+#Create Folders
+foreach ($folder in $FoldersToMake){
+    #$fullFolderPath = $file.ParentPath + "\Files"
+    $fullFolderPath = $ExportPath +"\$($folder)"
+    $fullFilesPath = $ExportPath +"\$($folder)\Files"
+    if (-not (Test-Path $fullFilesPath)){
+        New-Item -Path $fullFolderPath  -Name "Files" -ItemType "directory"
+    }
+}
+
+#Downloading Files
+foreach ($file in $files)
+{
+    Write-Host -ForegroundColor Yellow "$(Get-TimeStamp) Downloading $($file.Name)."
+    $fullFilePath = $file.ParentPath + "\Files\" + $file.Name
+        if (-not (Test-Path $fullFilePath)){
+            try{
+                $request = (New-Object System.Net.WebClient).DownloadFile($file.DownloadURL, $fullFilePath)
+            }
+            catch [System.Net.WebException]{
+                Write-Host -ForegroundColor Red "$(Get-TimeStamp) Error: Unable to download $($file.Name) to $($fullFilePath)"
+            }   
+        }
+        else {
+            try{
+                $extensionPosition = $file.name.LastIndexOf('.')
+                $splitFileName = $file.name.Substring(0,$extensionPosition)
+                $splitFileExtention = $file.name.Substring($extensionPosition)
+                $newFileName = $splitFileName + $file.Time + $splitFileExtention
+                $fullFilePath = $file.ParentPath + "\Files\" + $newFileName
+                $request = (New-Object System.Net.WebClient).DownloadFile($file.DownloadURL, $fullFilePath)
+            }
+            catch [System.Net.WebException]{
+                Write-Host -ForegroundColor Red "$(Get-TimeStamp) Error: Unable to download $($file.Name) to $($fullFilePath)"
+            }   
+        }
+}
+Write-Host -ForegroundColor Green "$(Get-TimeStamp) Step 2 of 2 complete. `n"
+Write-Host -ForegroundColor Green "$(Get-TimeStamp) Exiting.."
